c7b5a77e2ee945018db833c9063beb1fc06ca0f5
vikassinghrathor <rathorevikas2809@gmail.com>
2023-10-15 13:27:30 +0530

    save and get previous user information on website

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function savetoaxios(event){
+function savetoaxios(event) {
   event.preventDefault();
   const name = event.target.username.value;
   const email = event.target.emailId.value;
@@ -7,18 +7,46 @@ function savetoaxios(event){
   const obj = {
     name,
     email,
-    phonenumber
-  }
+    phonenumber,
+  };
 
-  axios.post("https://crudcrud.com/api/e40d4e4babcc401abc0a8aea6b7efc3e/appoinmentData",obj)
-  .then((response)=>{
-    console.log(response)
-  })
-  .catch((error)=>{
-    console.log(error)
-  })
-
-
-
-  showNewUserOnScreen(obj)
+  axios
+    .post(
+      "https://crudcrud.com/api/e40d4e4babcc401abc0a8aea6b7efc3e/appoinmentData",
+      obj
+    )
+    .then((response) => {
+      console.log(response);
+      showNewUserOnScreen(obj);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
+
+function showNewUserOnScreen(user) {
+  const userList = document.getElementById("userList");
+  const listItem = document.createElement("li");
+  listItem.textContent = `Name: ${user.name}, Email: ${user.email}, Phone: ${user.phonenumber}`;
+  userList.appendChild(listItem);
+}
+
+// Load saved users when the page loads
+window.addEventListener("DOMContentLoaded", () => {
+  axios
+    .get(
+      "https://crudcrud.com/api/e40d4e4babcc401abc0a8aea6b7efc3e/appoinmentData"
+    )
+    .then((response) => {
+      console.log(response);
+      for (let i = 0; i < response.data.length; i++) {
+        showNewUserOnScreen(response.data[i]);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+const form = document.getElementById("userForm");
+form.addEventListener("submit", savetoaxios);
