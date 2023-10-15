5bd34b99c96df7fea18cf72e7d165680a70f2eb3
vikassinghrathor <rathorevikas2809@gmail.com>
2023-10-15 15:07:08 +0530

    update

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ function savetoaxios(event) {
     .then((response) => {
       console.log(response);
       showNewUserOnScreen(obj, response.data._id); // Pass the user ID from the response
+      resetForm();
     })
     .catch((error) => {
       console.log(error);
@@ -29,9 +30,31 @@ function showNewUserOnScreen(user, userId) {
   const listItem = document.createElement("li");
   listItem.innerHTML = `
     Name: ${user.name}, Email: ${user.email}, Phone: ${user.phonenumber}
+    <button onclick="editUser('${userId}')">Edit</button>
     <button onclick="deleteUser('${userId}')">Delete</button>`;
   listItem.id = userId; // Set the ID for the list item
   userList.appendChild(listItem);
+}
+
+function resetForm() {
+  const form = document.getElementById("userForm");
+  form.reset();
+}
+
+function editUser(userId) {
+  axios
+    .get(
+      `https://crudcrud.com/api/e40d4e4babcc401abc0a8aea6b7efc3e/appoinmentData/${userId}`
+    )
+    .then((response) => {
+      const user = response.data;
+      document.getElementById("username").value = user.name;
+      document.getElementById("emailId").value = user.email;
+      document.getElementById("phonenumber").value = user.phonenumber;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 // Function to delete a user by their ID
@@ -44,6 +67,7 @@ function deleteUser(userId) {
       // Remove the deleted user from the list by ID
       const userItem = document.getElementById(userId);
       userItem.remove();
+      resetForm();
     })
     .catch((error) => {
       console.log(error);
